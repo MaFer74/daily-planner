4e5f1a2ea324005a7b5132897b2a45c97110aca8
Fernanda <152433962+MaFer74@users.noreply.github.com>
2024-03-12 18:25:26 +0000

    Completed css

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ $("#currentDay") .text(dayjs().format("ddd MMM D, YYYY"))
 $(".time-block").each(function() {
     console.log($(this).attr("id"));
     // Extract the hour from the id, assuming the format is "hour-X"
-    var hour = parseInt($(this).attr("id").split("-")[1]);
+    var hour = parseInt($(this).attr("id"));
     var currentHour = dayjs().hour(); // Get the current hour
+    console.log(hour);
+    console.log(currentHour);
 
     // Compare the hour of the .time-block with the current hour
     if (hour === currentHour) {
         // If the hour matches the current hour, add a class or perform other actions
-        $(this).addClass("current-hour");
+        $(this).addClass("present");
     } else if (hour < currentHour) {
         // If the hour is in the past, add a class or perform other actions
-        $(this).addClass("past-hour");
+        $(this).addClass("past");
     } else {
         // If the hour is in the future, add a class or perform other actions
-        $(this).addClass("future-hour");
+        $(this).addClass("future");
     }
 });
